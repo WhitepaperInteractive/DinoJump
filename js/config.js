9302cf15ed40dc88cb27b2eaabc8cd989699d4a3
WhitepaperInteractive <whitepaperinteractive@gmail.com>
2025-12-11 09:23:24 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,8 @@
-// SATSNAKE_CONFIG
 const SATSNAKE_CONFIG = {
-  // recipient Lightning Address (your requirement)
   recipientLightningAddress: "mustardmoose1@primal.net",
-
-  // If available, the recipient's nostr pubkey. If empty, we'll try to resolve via LUD-16.
   recipientNostrPubkey: "",
-
-  // Required amount to unlock (sats)
   minPaymentSats: 21,
 
-  // Nostr relays to listen on
   relays: [
     "wss://relay.damus.io",
     "wss://nos.lol",
@@ -17,12 +10,14 @@ const SATSNAKE_CONFIG = {
     "wss://relay.nostr.band"
   ],
 
-  zapReceiptTimeout: 60_000, // how long to wait for zap receipt (ms)
-  relayTimeout: 5000,        // relay connect timeout (ms)
+  zapReceiptTimeout: 60_000,
+  relayTimeout: 5000,
 
-  ui: {
-    showQrCode: true
-  }
+  CORS_PROXIES: [
+    "https://cors.isomorphic-git.org/",
+    "https://api.allorigins.win/raw?url=",
+    "https://corsproxy.io/?"
+  ],
+
+  ui: { showQrCode: true }
 };
-
-if (typeof module !== 'undefined' && module.exports) module.exports = SATSNAKE_CONFIG;
